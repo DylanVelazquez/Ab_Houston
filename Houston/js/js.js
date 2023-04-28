3e5d64e4bae84a67773490811d30b163643c1412
DylanVelazquez <128100666+DylanVelazquez@users.noreply.github.com>
2023-04-28 15:50:41 +0000

    Update js.js

--- Houston/js/js.js
+++ Houston/js/js.js
@@ -1,21 +1,30 @@
-let n = parseInt(prompt("Ingrese la longitud de los arrays :"));
-let arr1 = [];
-let arr2 = [];
+function generarVectores(longitud) {
+  let arr1 = [];
+  let arr2 = [];
 
-for (let i = 0; i < n; i++) {
-  arr1.push(Math.floor(Math.random() * 30000));
-  arr2.push(Math.floor(Math.random() * 30000));
+
+  for (let i = 0; i < longitud; i++) {
+    const numAleatorio = Math.floor(Math.random() * 30000);
+    arr1.push(numAleatorio);
+    arr2.push(numAleatorio);
+  }
+
+  let arr3 = [];
+
+  
+  for (let i = 0; i < longitud; i++) {
+    arr3.push(arr1[i] * arr2[i]);
+  }
+
+ 
+  let sum = arr3.reduce((acc, val) => acc + val);
+
+  
+  console.log("Array 1: ", arr1);
+  console.log("Array 2: ", arr2);
+  console.log("Array 3 (resultado de la multiplicación): ", arr3);
+  console.log("Suma de los elementos de Array 3: ", sum);
 }
 
-let arr3 = [];
-
-for (let i = 0; i < n; i++) {
-  arr3.push(arr1[i] * arr2[i]);
-}
-
-let sum = arr3.reduce((acc, val) => acc + val);
-
-console.log("Array 1: ", arr1);
-console.log("Array 2: ", arr2);
-console.log("Array 3 (resultado de la multiplicación): ", arr3);
-console.log("Suma de los elementos de Array 3: ", sum);
+let n = parseInt(prompt("Ingrese la longitud de los arrays (no mayor a 7):"));
+generarVectores(n);
